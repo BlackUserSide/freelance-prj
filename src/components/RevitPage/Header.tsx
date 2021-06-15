e4f97f617c8d5e2904c1e0be3cc48e7129d5c012
BlackUserSide <nesmserg@gmail.com>
2021-06-15 15:48:23 +0300

    restore

--- src/components/RevitPage/Header.tsx
+++ src/components/RevitPage/Header.tsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 export const Header: React.FC = () => {
+  const history = useHistory();
+
   return (
     <header className="page-header">
       <nav className="main-nav">
         <ul>
           <li>
             <div className="close-border"></div>
-            <div className="arrow-close">
+            <div
+              className="arrow-close"
+              onClick={() => {
+                history.goBack();
+              }}
+            >
               <div className="arrow"></div>
             </div>
           </li>
@@ -16,9 +24,11 @@ export const Header: React.FC = () => {
             <a href="/#">Чому навчимо</a>
           </li>
           <li>
+            <div className="close-border"></div>
             <a href="/#">Роботи студентів</a>
           </li>
           <li>
+            <div className="close-border"></div>
             <a href="/#">Куратор</a>
           </li>
         </ul>
