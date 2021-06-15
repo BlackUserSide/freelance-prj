e8e07013efb4b959809aeb342ad57b52fb483fe7
filatovnikita27 <filatovnikita27@gmail.com>
2021-06-15 18:38:42 +0300

    commit

--- src/components/RevitPage/MainRavinPage.tsx
+++ src/components/RevitPage/MainRavinPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { LastScreen } from "../LastScreen/LastScreen";
 import { Header } from "./Header";
 import "./revit.sass";
 export const MainRavinPage: React.FC = () => {
   return (
     <div className="main-ravin-page">
       <Header />
+      <LastScreen/>
     </div>
   );
 };
