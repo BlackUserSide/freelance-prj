19f19b7f358941c03e80497090399e03967d7a27
filatovnikita27 <filatovnikita27@gmail.com>
2021-06-15 15:44:12 +0300

    inital_fil-commit

---
+++ src/components/LastScreen/LastScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './lastScreen.sass'
+
+export const LastScreen:React.FC = () => {
+    return (
+        <div className="last-screen">
+            <div className="last-screen_container"></div>
+            <div className="last-screen_container"></div>
+        </div>
+    )
+}
